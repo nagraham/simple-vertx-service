8d1a4e635b97808c90c06c09f579a7d2fdfdcdaa
Alex Graham <nagraham00@gmail.com>
2019-06-14 08:39:26 -0700

    Use vertx config to retrieve http server config from JSON file

--- src/main/java/com/alexco/simplevertxservice/MainVerticle.java
+++ src/main/java/com/alexco/simplevertxservice/MainVerticle.java
@@ -2,9 +2,13 @@ package com.alexco.simplevertxservice;
 
 import com.alexco.simplevertxservice.database.UserDatabaseVerticle;
 import com.alexco.simplevertxservice.user.UserHttpServerVerticle;
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 
 /**
@@ -15,13 +19,31 @@ public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> startFuture) {
-        deployUserDatabaseVerticle().compose(id -> deployHttpServerVerticle()).setHandler(asyncResult -> {
-            if (asyncResult.succeeded()) {
-                startFuture.complete();
+        getConfigRetriever().getConfig(configAsyncResult -> {
+            if (configAsyncResult.succeeded()) {
+                deployUserDatabaseVerticle().compose(id -> deployHttpServerVerticle(configAsyncResult.result()))
+                        .setHandler(asyncResult -> {
+                            if (asyncResult.succeeded()) {
+                                startFuture.complete();
+                            } else {
+                                startFuture.fail(asyncResult.cause());
+                            }
+                        });
             } else {
-                startFuture.fail(asyncResult.cause());
+                startFuture.fail(configAsyncResult.cause());
             }
         });
+    }
+
+    private ConfigRetriever getConfigRetriever() {
+        Future<JsonObject> configFuture = Future.future();
+
+        ConfigStoreOptions fileStore = new ConfigStoreOptions()
+                .setType("file")
+                .setConfig(new JsonObject().put("path", "src/main/resources/development.json"));
+        ConfigRetrieverOptions options = new ConfigRetrieverOptions().addStore(fileStore);
+
+        return ConfigRetriever.create(vertx, options);
     }
 
     private Future<String> deployUserDatabaseVerticle() {
@@ -30,16 +52,12 @@ public class MainVerticle extends AbstractVerticle {
         return future;
     }
 
-    private Future<String> deployHttpServerVerticle() {
+    private Future<String> deployHttpServerVerticle(JsonObject config) {
         Future<String> future = Future.future();
-
-        // TODO: refactor config to be read in from JSON file
-        JsonObject httpServerConfig = new JsonObject()
-                .put(UserHttpServerVerticle.CONFIG_HTTP_PORT, 8080);
 
         // deploy 3 instances (b/c why the hell not? ... vertx round-robins requests on port 8080 to each instance)
         DeploymentOptions deploymentOptions = new DeploymentOptions()
-                .setConfig(httpServerConfig)
+                .setConfig(config)
                 .setInstances(3);
 
         vertx.deployVerticle(USER_HTTP_VERTICLE, deploymentOptions, future);
